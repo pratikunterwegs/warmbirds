ab234c2c55dc4b8ea6d1c62c54f64f5dcdb09851
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-11 19:27:33 +0100

    stats per groups

--- read_data.jl
+++ read_data.jl
@@ -6,8 +6,8 @@ using Revise
 # load libraries
 using CSV
 using DataFrames 
-using Gadfly
-using StatsBase
+using StatsBase # required for mean. wtf
+using Statistics
 using StatsPlots
 
 # read in species traits
@@ -16,5 +16,10 @@ data_trait = CSV.read("data/data_species_trait.csv", DataFrame)
 # count by multiple columns
 combine(groupby(data_trait, [:range_size, :habitat]), nrow => :count)
 
-# show boxplot
+# get max bodymass by each groupby
+combine(groupby(data_trait, [:range_size, :habitat]), 
+                    :body_mass .=> [mean, maximum, std])
 
+# try reading a very large file
+# it's alright, not too bad
+data = CSV.read("../eBirdOccupancy/data/ebird_for_expertise.txt", DataFrame)
