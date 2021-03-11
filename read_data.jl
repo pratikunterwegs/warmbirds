516f3a807e6518420f9b98f8eb6d8933c6acac04
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-11 22:25:17 +0100

    using atom, add coord summary across species

--- read_data.jl
+++ read_data.jl
@@ -1,14 +1,10 @@
 # read data in julia
 
-# load revise
-using Revise
-
 # load libraries
 using CSV
-using DataFrames 
+using DataFrames
 using StatsBase # required for mean. wtf
 using Statistics
-using StatsPlots
 
 # read in species traits
 data_trait = CSV.read("data/data_species_trait.csv", DataFrame)
@@ -17,9 +13,19 @@ data_trait = CSV.read("data/data_species_trait.csv", DataFrame)
 combine(groupby(data_trait, [:range_size, :habitat]), nrow => :count)
 
 # get max bodymass by each groupby
-combine(groupby(data_trait, [:range_size, :habitat]), 
+combine(groupby(data_trait, [:range_size, :habitat]),
                     :body_mass .=> [mean, maximum, std])
 
 # try reading a very large file
 # it's alright, not too bad
 data = CSV.read("../eBirdOccupancy/data/ebird_for_expertise.txt", DataFrame)
+
+# get the max elev per species per year per month
+names(data)
+rename!(data, replace.(names(data), " " => "_"))
+coord_summary = combine(groupby(data, [:SCIENTIFIC_NAME]),
+            [:LATITUDE] .=> [mean, maximum],
+            [:LONGITUDE] .=> [mean, maximum])
+
+# save to file
+CSV.write("data/data_coord_summary_test.csv", coord_summary)
