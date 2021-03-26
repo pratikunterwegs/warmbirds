4ae670aca65e545c3307f990ebe2ab3d00c42d1a
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-26 11:41:47 +0100

    remove old

--- scripts/using_r.jl
+++
@@ -1,8 +0,0 @@
-# code to try reading shapefiles and use endemicr
-using RCall
-
-a = R"(rnorm(10))" # the r string macro
-
-R"scico::scico(n = 9, palette = 'berlin')" # hahaha this is great!
-
-a = R"raster::raster('data/kruger_temperature_UTM.tif')"
