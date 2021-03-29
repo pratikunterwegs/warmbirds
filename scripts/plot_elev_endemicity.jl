dd4e18139e84823247cf2481efb1549c6e60e556
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-29 22:49:28 +0200

    do plots by endemicity

--- scripts/plot_elev_endemicity.jl
+++ scripts/plot_elev_endemicity.jl
@@ -3,30 +3,45 @@
 using CSV
 using DataFrames
 using RCall
+using Gadfly
+using Statistics
+using StatsBase
 
 # read data
 data = CSV.read("data/output/data_species_elevation_summary.csv", DataFrame)
+data_endemic = CSV.read("data/output/data_endemicity.csv", DataFrame)
+# combine
+data = leftjoin(data, data_endemic, on = :scientific_name => :sciname)
 
-set_default_plot_size(16cm, 9cm)
+# filter missing
+filter!(row -> (!ismissing(row.p_range)) & (row.seasonality != 4), data)
+
+# summarise data
+data.endemicity_round = round.(data.p_range, digits = 2)
+data_elev_endem_summary = combine(groupby(data, [:seasonality, :endemicity_round]),
+        :elevation_mean .=> [mean, std])
+
+set_default_plot_size(25cm, 18cm)
 # plot lines
 plot(data,
     y = :elevation_mean,
-    x = :month,
-    color = :scientific_name,
-    Geom.point,
+    x = :endemicity_round,
+    xgroup = :seasonality,
+    color = :seasonality,
+    Geom.subplot_grid(
+        Geom.boxplot,
+        Coord.cartesian(
+        ymin = 0
+        ),
+    ),
+    Scale.x_sqrt,
+    Scale.color_discrete,
     Theme(
-        background_color = "white",
-        key_position = :none
+        background_color = "white"
     )
-) # nice, but no ggplot
+)
 
-# use ggplot because why use something substandard
-R"library(ggplot2)"
-R"ggplot($data)+
-    geom_path(aes(month, elevation_mean,
-                    col = scientific_name),
-                    show.legend = FALSE)+
-    facet_grid(~year)"
+
 
 # plot elevation mode change
 data = CSV.read("data/output/data_species_elev_mode.csv", DataFrame)
