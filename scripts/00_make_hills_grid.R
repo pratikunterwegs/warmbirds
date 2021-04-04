48a3c5e51bd2aec71b899266df74b5c162cb0555
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-04 16:42:01 +0200

    hills grid code

--- scripts/00_make_hills_grid.R
+++ scripts/00_make_hills_grid.R
@@ -6,7 +6,7 @@ library(stars)
 
 # get raster
 elevation = raster("data/spatial/elevation_sasia.tif")
-plot(elevation, col = sequential_hcl(20, palette = "Heat2", rev = T))
+plot(elevation, col = sequential_hcl(20, palette = "Sunset", rev = T))
 
 # subset areas with values > 800
 hills_sasia = elevation
@@ -33,10 +33,11 @@ hills_sf_buffer = st_transform(hills_sf_buffer, 4326)
 st_write(hills_sf_buffer, dsn = "data/spatial/hills_sasia_buffer.gpkg")
 
 # make grid
-hills_grid = st_make_grid(hills_buffer, 
-                          cellsize = 50*1e3, square = F, what = "polygons")
+hills_sf_buffer = st_transform(hills_sf_buffer, 32643)
+hills_grid = st_make_grid(hills_sf_buffer, 
+                          cellsize = 100*1e3, square = F, what = "polygons")
 # subset by the hills buffer
-intersect_grid_hill = st_intersects(hills_buffer, hills_grid)
+intersect_grid_hill = st_intersects(hills_sf_buffer, hills_grid)
 intersect_grid_hill = unique(unlist(unclass(intersect_grid_hill)))
 
 hills_grid = hills_grid[intersect_grid_hill]
@@ -49,4 +50,4 @@ hills_grid$polygon_number = seq(nrow(hills_grid))
 hills_grid = st_transform(hills_grid, 4326)
 
 # save to file
-st_write(hills_grid, dsn = "data/spatial/hills_sasia_grid.gpkg")
+st_write(hills_grid, dsn = "data/spatial/hills_sasia_grid_100km.gpkg")
