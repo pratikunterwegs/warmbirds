3d62d07305b700904b6779e026648c54a6fbed50
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-05 12:22:21 +0200

    filter script rename

--- scripts/03_filter_species.jl
+++ scripts/03_filter_species.jl
@@ -4,7 +4,7 @@ using CategoricalArrays
 using DataFrames
 
 # read data and remove genus level, uncertain species, and domestic birds
-data = CSV.read("data/output/data_observation_elevatio.csv", DataFrame)
+data = CSV.read("data/output/data_observation_elevation.csv", DataFrame)
 filter!(row -> !(occursin(r"(sp.)|(omestic)|(/)", row.scientific_name)), data)
 
 CSV.write("data/output/data_observation_elevation.csv", data)
